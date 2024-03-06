1b543d41754af04672a2cfd8277d616a1015c78a
cdixon12 <chasedixon2@gmail.com>
2024-03-05 21:09:54 -0500

    back4app ids

---
+++ environment.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: "RBC6wL2P9Y1WT6fW2NI92HWmzg6FoVhN5d2uvtOh",
+  JAVASCRIPT_KEY: "uEGQPi1BsW5HeZ9Ol2FBWWaSzn1UxF112w1PsxAv",
+  SERVER_URL: "https://parseapi.back4app.com/"
+};
